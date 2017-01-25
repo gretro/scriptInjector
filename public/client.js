6b6ed6d4677c049325b6f2f081a6a9a7891564aa
Gabriel Lemire <gabriel.lemire@gmail.com>
2017-01-25 00:24:52 -0500

    Inject script into the page

---
+++ public/client.js
@@ -0,0 +1,3 @@
+(() => {
+    console.log('Injected script executed');
+})();
--- scriptInjector.js
+++ scriptInjector.js
@@ -1,3 +1,10 @@
 (() => {
-    console.log('Content script loaded and ready!');
+    var s = document.createElement('script');
+    // TODO: add "script.js" to web_accessible_resources in manifest.json
+    s.src = 'http://localhost:8080/public/client.js';
+    s.onload = function() {
+        this.remove();
+    };
+
+    (document.head || document.documentElement).appendChild(s);
 })();
